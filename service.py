a4405f291f09ad3a1a9ae56ebd134e3418237cea
Vergangenheit <lorenzo.ostano@gmail.com>
2020-04-27 10:26:29 +0200

    added caching of credentials

--- service.py
+++ service.py
@@ -3,12 +3,27 @@ import config
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import os
+import pickle
 
 
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(config.CLIENT_SECRETS_FILE, config.SCOPES)
-    credentials = flow.run_console()
+    credentials = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as f:
+            credentials = pickle.load(f)
+    #  Check if the credentials are invalid or do not exist
+    if not credentials or not credentials.valid:
+        # Check if the credentials have expired
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(config.CLIENT_SECRETS_FILE, config.SCOPES)
+            credentials = flow.run_console()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as f:
+            pickle.dump(credentials, f)
 
     return build(config.API_SERVICE_NAME, config.API_VERSION, credentials=credentials)
 
