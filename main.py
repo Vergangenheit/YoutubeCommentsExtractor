9de84e5cc822f83310970ecbbe3ebf3fe4b32f7f
Vergangenheit <lorenzo.ostano@gmail.com>
2020-04-27 17:23:30 +0200

    completed scripts with storing results to csv

---
+++ main.py
@@ -0,0 +1,10 @@
+from service import get_authenticated_service
+from searches import *
+
+if __name__ == "__main__":
+    # When running locally, disable OAuthlib's HTTPs verification. When
+    # running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    service = get_authenticated_service()
+    keyword = input('Enter a keyword: ')
+    search_videos_comments_by_keyword(service=service, q=keyword, part='id, snippet', eventType='completed', type='video')
---
+++ searches.py
@@ -0,0 +1,73 @@
+from service import get_authenticated_service
+from store_comments import write_to_csv
+import os
+
+
+def get_videos(service, **kwargs):
+    final_results = []
+    results = service.search().list(**kwargs).execute()
+
+    i = 0
+    max_pages = 3
+    while results and i < max_pages:
+        final_results.extend(results['items'])
+
+        # check if another page exists
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = service.search().list(**kwargs).execute()
+            i += 1
+        else:
+            break
+
+    return final_results
+
+
+def get_video_comments(service, **kwargs):
+    comments = []
+    try:
+        results = service.commentThreads().list(**kwargs).execute()
+
+        while results:
+            for item in results['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                comments.append(comment)
+
+            if 'nextPageToken' in results:
+                kwargs['pageToken'] = results['nextPageToken']
+                results = service.commentThreads().list(**kwargs).execute()
+            else:
+                break
+
+    except Exception as e:
+        print(e)
+
+    return comments
+
+
+def search_videos_by_keyword(service, **kwargs):
+    results = get_videos(service, **kwargs)
+    for item in results:
+        print('%s - %s' % (item['snippet']['title'], item['id']['videoId']))
+
+
+def search_videos_comments_by_keyword(service, **kwargs):
+    results = get_videos(service, **kwargs)
+    final_results = []
+    for item in results:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        print('%s - %s' % (title, video_id))
+        comments = get_video_comments(service, part='snippet', videoId=video_id, textFormat='plainText')
+        final_results.extend([(video_id, title, comment) for comment in comments])
+        #print(comments)
+
+    write_to_csv(final_results)
+
+
+if __name__ == "__main__":
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    service = get_authenticated_service()
+    keyword = input('Enter a keyword: ')
+    search_videos_comments_by_keyword(service=service, q=keyword, part='id, snippet', eventType='completed',
+                                      type='video')
