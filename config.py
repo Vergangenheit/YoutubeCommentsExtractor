d7ff02ba0d2659b18cb3ecd3106512447b9d7e96
Loloheia <lorenzo.ostano@gmail.com>
2021-04-16 08:48:03 +0200

    updated gitignore

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ CLIENT_SECRETS_FILE = "client_secret.json"
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
+API_KEY = 'AIzaSyCV84ID3XDGe6KmobcjKnqwzD7lyFk05ZM'
