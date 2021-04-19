a114086ed79b82a14e78333f3ff501eac322e939
Loloheia <lorenzo.ostano@gmail.com>
2021-04-19 09:04:26 +0200

    started building solution with async queue

--- async_extractor/extractor.py
+++ async_extractor/extractor.py
@@ -1,65 +1,88 @@
 import asyncio
-from asyncio import AbstractEventLoop
+from asyncio import Queue, Task, AbstractEventLoop
 from aiogoogle import Aiogoogle, GoogleAPI
 from aiogoogle.models import Response, Request
 import config
 from typing import List, Dict, Coroutine, Any
 import time
+import csv
+import codecs
+import os
 
 
-async def get_videos(q: str, part: str, eventType: str, type: str) -> Dict:
+async def get_videos(k: str, part: str, eventType: str, type: str, q: Queue) -> None:
     async with Aiogoogle(api_key=config.API_KEY) as aiog:
         youtube: GoogleAPI = await aiog.discover('youtube', 'v3')
-        req: Request = youtube.search.list(q=q, part=part, eventType=eventType, type=type)
+        req: Request = youtube.search.list(q=k, part=part, eventType=eventType, type=type)
         res: Dict = await aiog.as_api_key(req)
-    videos: List[Dict] = res.get('items')
-    # print(videos)
+    items: List[Dict] = res.get('items')
+    n: int = len(items)
+    for item in items:
+        videoId: str = item.get('id').get('videoId')
+        t: float = time.perf_counter()
+        await q.put((videoId, t))
+        print(f"Producer added <{videoId}> to queue. at time {t}")
 
-    return videos
 
-
-async def get_video_comments(videoId: str, textFormat: str) -> List:
+async def get_video_comments(videoId: str, textFormat: str, q: Queue) -> None:
     comments: List = []
-    async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
-        youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
-        req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
-        results: Dict = await aiocomments.as_api_key(req)
+    while True:
+        async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
+            youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
+            req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
+            results: Dict = await aiocomments.as_api_key(req)
 
-    for item in results.get('items'):
-        comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
-        comments.append(comment)
+        for item in results.get('items'):
+            comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            t: float = time.perf_counter()
+            await q.put((comment, t))
 
-    return comments
 
-
-async def get_video_comments_multiples(videoId: str, textFormat: str):
+async def get_video_comments_multiples(textFormat: str, q: Queue) -> None:
     comments: List = []
-    async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
-        youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
-        req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
-        results: Coroutine[Dict] = await aiocomments.as_api_key(req)
-        while results:
-            for item in results.get('items'):
-                comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
-                comments.append(comment)
-            if 'nextPageToken' in results:
-                pageToken = results['nextPageToken']
-                req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat,
-                                                  pageToken=pageToken)
-                results: Dict = await aiocomments.as_api_key(req)
-            else:
-                break
+    while True:
+        async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
+            youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
+            videoId, t = await q.get()
+            req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
+            results: Coroutine[Dict] = await aiocomments.as_api_key(req)
+            while results:
+                for item in results.get('items'):
+                    comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                    # put comment in queue
+                    t: float = time.perf_counter()
+                    await q.put((comment, t))
+                if 'nextPageToken' in results:
+                    pageToken = results['nextPageToken']
+                    req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat,
+                                                      pageToken=pageToken)
+                    results: Dict = await aiocomments.as_api_key(req)
+                else:
+                    break
 
-    print(comments)
+
+def write_to_csv(output_filename: str, comments: List[str]):
+    """
+        Write the extracted content into the file
+    """
+    try:
+        with codecs.open(output_filename, 'w', encoding='utf-8', errors='ignore') as comments_file:
+            comments_writer = csv.writer(comments_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in comments:
+            comments_writer.writerow(list(row))
+    except FileNotFoundError:
+        print("Output file not present", output_filename)
+        print("Current dir: ", os.getcwd())
+        raise FileNotFoundError
 
 
-# def main(**kwargs):
-#     # create loop
-#     t0: float = time.perf_counter()
-#     loop: AbstractEventLoop = asyncio.get_event_loop()
-#     loop.run_until_complete(search_videos_comments_by_keyword(loop, **kwargs))
-#     t1: float = time.perf_counter()
-#     print(f"Done in {t1 - t0}")
+def main(keyword: str):
+    loop: AbstractEventLoop = asyncio.get_running_loop()
+    q = asyncio.Queue()
+    video_prod: Task = asyncio.create_task(
+        get_videos(k=keyword, part='id, snippet', eventType='completed', type='video', q=q))
+    comment_prod: Task = asyncio.create_task(get_video_comments_multiples(textFormat='plainText', q=q))
+
 
 
 if __name__ == "__main__":
