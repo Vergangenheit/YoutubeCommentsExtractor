4b54cdd82d43be9a058bd8d3abf6da5c14100a1b
Loloheia <lorenzo.ostano@gmail.com>
2021-04-20 08:58:54 +0200

    compared with sync code, about 82 seconds difference

--- extractor/searches.py
+++ extractor/searches.py
@@ -4,7 +4,8 @@ import os
 from googleapiclient.discovery import Resource
 import time
 
-def get_videos(service, max_pages, **kwargs):
+
+def get_videos(service: Resource, max_pages: int, **kwargs):
     final_results = []
     results = service.search().list(**kwargs).execute()
 
@@ -58,7 +59,7 @@ def get_video_comments_multiples(service: Resource, videoId: str, textFormat: st
             if 'nextPageToken' in results:
                 pageToken = results['nextPageToken']
                 results = service.commentThreads().list(videoId=videoId, part='snippet', textFormat=textFormat,
-                                                  pageToken=pageToken).execute()
+                                                        pageToken=pageToken).execute()
             else:
                 break
 
@@ -74,7 +75,7 @@ def search_videos_by_keyword(service, **kwargs):
         print('%s - %s' % (item['snippet']['title'], item['id']['videoId']))
 
 
-def search_videos_comments_by_keyword(service, max_pages, **kwargs):
+def search_videos_comments_by_keyword(service: Resource, max_pages: int, write: bool, **kwargs):
     results = get_videos(service, max_pages, **kwargs)
     final_results = []
     for item in results:
@@ -83,9 +84,12 @@ def search_videos_comments_by_keyword(service, max_pages, **kwargs):
         print('%s - %s' % (title, video_id))
         comments = get_video_comments(service, part='snippet', videoId=video_id, textFormat='plainText')
         final_results.extend([(video_id, title, comment) for comment in comments])
-        # print(comments)
 
-    write_to_csv(final_results)
+    print(final_results[:10])
+    print(len(final_results))
+
+    if write:
+        write_to_csv(final_results)
 
 
 if __name__ == "__main__":
@@ -93,9 +97,10 @@ if __name__ == "__main__":
     # keyword = input('Enter a keyword: ')
     t0: float = time.perf_counter()
     service = get_authenticated_service()
-    get_video_comments_multiples(service, videoId='5tSTk1083VY', textFormat='plainText')
+
+    search_videos_comments_by_keyword(service=service, max_pages=1, write=False, q='David Goggins', part='id, snippet',
+                                      eventType='completed',
+                                      type='video')
     t1: float = time.perf_counter()
     print(f"Done in {t1 - t0}")
-    # search_videos_comments_by_keyword(service=service, max_pages=1, q=keyword, part='id, snippet',
-    #                                   eventType='completed',
-    #                                   type='video')
+    # done in 220.2 secs
