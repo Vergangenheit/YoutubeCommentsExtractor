bfba2b4d55a294298172a516fdc21741f3d6c4fd
Vergangenheit <lorenzo.ostano@gmail.com>
2022-02-02 13:42:10 +0100

    yep

--- async_extractor/extractor.py
+++ async_extractor/extractor.py
@@ -3,14 +3,21 @@ from asyncio import Queue, Task, AbstractEventLoop
 from aiogoogle import Aiogoogle, GoogleAPI
 from aiogoogle.models import Response, Request
 import config
-from typing import List, Dict, Coroutine, Any
+from typing import List, Dict, Coroutine, Any, Tuple
 import time
 import csv
 import codecs
 import os
+from multiprocessing import cpu_count
+import concurrent.futures  # Allows creating new processes
+from concurrent.futures import ProcessPoolExecutor, Future
+import logging
+import random
+import colorama
+import aiofiles
 
 
-async def get_videos(k: str, part: str, eventType: str, type: str) -> str:
+async def get_videos(k: str, part: str, eventType: str, type: str, data: Queue):
     async with Aiogoogle(api_key=config.API_KEY) as aiog:
         youtube: GoogleAPI = await aiog.discover('youtube', 'v3')
         req: Request = youtube.search.list(q=k, part=part, eventType=eventType, type=type)
@@ -19,7 +26,10 @@ async def get_videos(k: str, part: str, eventType: str, type: str) -> str:
     n: int = len(items)
     for item in items:
         videoId: str = item.get('id').get('videoId')
-        return videoId
+        title: str = item.get('snippet').get('title')
+        logging.info(colorama.Fore.YELLOW + "Got videos %s - %s", videoId, title)
+        await data.put((videoId, title))
+        await asyncio.sleep(random.random() + .1)
 
 
 async def get_video_comments(videoId: str, textFormat: str, q: Queue) -> None:
@@ -36,58 +46,70 @@ async def get_video_comments(videoId: str, textFormat: str, q: Queue) -> None:
             await q.put((comment, t))
 
 
-async def get_video_comments_multiples(k: str, part: str, eventType: str, type: str, textFormat: str) -> List:
-    comments: List = []
+async def get_video_comments_multiples(num: int, textFormat: str, data: Queue, comments: Queue):
+    processed = 0
+    while processed < num:
+        videoId, title = await data.get()
+        await asyncio.sleep(random.random() + .1)
+        async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
+            youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
+            req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
+            results: Coroutine[Dict] = await aiocomments.as_api_key(req)
+            while results:
+                for item in results.get('items'):
+                    comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                    logging.info(colorama.Fore.CYAN + "fetched comment %s", comment)
+                    # put comment in list
+                    await comments.put((videoId, title, comment))
+                    logging.info(colorama.Fore.LIGHTBLUE_EX + "stored comment %s", comment)
+                    await asyncio.sleep(random.random() + .5)
+                    if 'nextPageToken' in results:
+                        pageToken = results['nextPageToken']
+                        logging.info(colorama.Fore.BLUE + "going into next page %s", pageToken)
+                        req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat,
+                                                          pageToken=pageToken)
+                        results: Dict = await aiocomments.as_api_key(req)
+                    else:
+                        break
 
-    async with Aiogoogle(api_key=config.API_KEY) as aiocomments:
-        youtube: GoogleAPI = await aiocomments.discover('youtube', 'v3')
-        videoId = await get_videos(k, part, eventType, type)
-        req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat)
-        results: Coroutine[Dict] = await aiocomments.as_api_key(req)
-        while results:
-            for item in results.get('items'):
-                comment: str = item['snippet']['topLevelComment']['snippet']['textDisplay']
-                # put comment in list
-                comments.append(comment)
-            if 'nextPageToken' in results:
-                pageToken = results['nextPageToken']
-                req = youtube.commentThreads.list(videoId=videoId, part='snippet', textFormat=textFormat,
-                                                  pageToken=pageToken)
-                results: Dict = await aiocomments.as_api_key(req)
-            else:
-                break
-    print(comments[:10])
-    print(len(comments))
 
-
-def write_to_csv(output_filename: str, comments: List[str]):
+async def write_to_file(output_filename: str, comments: Queue):
     """
         Write the extracted content into the file
     """
-    try:
-        with codecs.open(output_filename, 'w', encoding='utf-8', errors='ignore') as comments_file:
-            comments_writer = csv.writer(comments_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in comments:
-            comments_writer.writerow(list(row))
-    except FileNotFoundError:
-        print("Output file not present", output_filename)
-        print("Current dir: ", os.getcwd())
-        raise FileNotFoundError
+
+    videoId, title, comment = await comments.get()
+    logging.info(colorama.Fore.LIGHTGREEN_EX + "Got comment %s", comment)
+    async with aiofiles.open(output_filename, 'a', encoding='utf-8', errors='ignore') as comments_file:
+        # comments_writer.writerow(['Video ID', 'Title', 'Comment'])
+        await comments_file.write(videoId + ', ' + title + ', ' + comment)
+        logging.info(colorama.Fore.WHITE + "Wrote comment %s", comment)
+            # except FileNotFoundError:
+            #     print("Output file not present", output_filename)
+            #     print("Current dir: ", os.getcwd())
+            #     raise FileNotFoundError
 
 
-async def main(keyword: str, part: str, eventType: str, type: str, textFormat: str):
-    await get_video_comments_multiples(keyword, part, eventType, type, textFormat)
+def run(keyword: str, part: str, eventType: str, type: str, textFormat: str) -> List:
+    comments = asyncio.run(get_video_comments_multiples(keyword, part, eventType, type, textFormat))
+    return comments
+
+
+def main(keyword: str, part: str, eventType: str, type: str, textFormat: str, num: int):
+    t0: float = time.perf_counter()
+    # create the asyncio loop
+    loop: AbstractEventLoop = asyncio.get_event_loop()
+    data = Queue()
+    comments = Queue()
+    task: Future = asyncio.gather(get_videos(k=keyword, part=part, eventType=eventType, type=type, data=data),
+                                  get_video_comments_multiples(num=num, textFormat=textFormat, data=data, comments=comments),
+                                  write_to_file('../comments.csv', comments=comments))
+    loop.run_until_complete(task)
+    t1: float = time.perf_counter()
+    print(f"Done in {t1 - t0}")
 
 
 if __name__ == "__main__":
-    # asyncio.run(get_videos(q='David Goggins', part='id, snippet', eventType='completed',
-    #                                   type='video'))
-    t0: float = time.perf_counter()
-    loop: AbstractEventLoop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        main(keyword='David Goggins', part='id, snippet', eventType='completed', type='video', textFormat='plainText'))
-    # asyncio.run(
-    #     main(keyword='David Goggins', part='id, snippet', eventType='completed', type='video', textFormat='plainText'))
-    t1: float = time.perf_counter()
-    print(f"Done in {t1 - t0}")
-    # Done in 138.7081521
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    main(keyword='David Goggins', part='id, snippet', eventType='completed', type='video', textFormat='plainText', num=4)
--- threading/extractor.py
+++ threading/extractor.py
@@ -1,1 +1,129 @@
 # TODO
+
+# !/usr/bin/env python3
+import concurrent.futures
+import logging
+from queue import Queue
+from typing import List, Optional
+import threading
+from threading import Event
+import time
+from googleapiclient.discovery import Resource, build
+import csv
+import codecs
+import config
+
+
+def get_authenticated_service() -> Resource:
+    service: Resource = build('youtube', 'v3', developerKey=config.API_KEY)
+
+    return service
+
+
+def get_videos(service: Resource, max_pages: int, k: str):
+    final_results = []
+    results = service.search().list(q=k, part='id, snippet', eventType='completed', type='video').execute()
+
+    i = 0
+    while results and i < max_pages:
+        final_results.extend(results['items'])
+
+        # check if another page exists
+        if 'nextPageToken' in results:
+            pageToken = results['nextPageToken']
+            results = service.search().list(q=k, part='id, snippet', eventType='completed', type='video',
+                                            pageToken=pageToken).execute()
+            i += 1
+        else:
+            break
+
+    return final_results
+
+
+def get_video_comments_multiples(service: Resource, videoId: str, textFormat: str) -> Optional[List]:
+    comments = []
+    try:
+        results = service.commentThreads().list(videoId=videoId, part='snippet', textFormat=textFormat).execute()
+
+        while results:
+            for item in results['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                comments.append(comment)
+
+            if 'nextPageToken' in results:
+                pageToken = results['nextPageToken']
+                results = service.commentThreads().list(videoId=videoId, part='snippet', textFormat=textFormat,
+                                                        pageToken=pageToken).execute()
+            else:
+                break
+        return comments
+    except Exception as e:
+        print(e)
+
+
+def producer(service: Resource, k: str, queue: Queue, event: Event):
+    """gets the videos"""
+    while not event.is_set():
+        results = get_videos(service, 1, k)
+        for item in results:
+            title = item['snippet']['title']
+            video_id = item['id']['videoId']
+            logging.info("Producer got video: %s %s", video_id, title)
+            queue.put((video_id, title))
+
+    logging.info("Producer received event. Exiting")
+
+
+def consumer(service: Resource, queue_videos: Queue, queue_comments: Queue, event_vid: Event, event_com: Event):
+    """ Pretend we're saving a number in the database. """
+    while not event_vid.is_set() or not queue_videos.empty():
+        video_id, title = queue_videos.get()
+        logging.info(
+            "Consumer storing message: %s (size=%d)", video_id + ' ' + title, queue_videos.qsize()
+        )
+        while event_com.is_set():
+            comments = get_video_comments_multiples(service, videoId=video_id, textFormat='plainText')
+            for comment in comments:
+                queue_comments.put((video_id, title, comment))
+                logging.info(
+                    "Consumer storing comment: %s (size=%d)", comment, queue_comments.qsize()
+                )
+
+    # print(len(final_results))
+    # print(final_results[:10])
+    logging.info("Consumer received event. Exiting")
+
+
+def writer(queue_comments: Queue, event: Event):
+    while not event.is_set() or not queue_comments.empty():
+        with codecs.open('../comments.csv', 'a', encoding='utf-8', errors='ignore') as comments_file:
+            comments_writer = csv.writer(comments_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            video_id, title, comment = queue_comments.get()
+            logging.info("Writing comment: %s", comment)
+            comments_writer.writerow([video_id, title, comment])
+
+    logging.info("Writer received event. Exiting")
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    t0: float = time.perf_counter()
+    youtube: Resource = get_authenticated_service()
+    queue_videos = Queue(maxsize=10)
+    queue_comments = Queue(maxsize=30)
+    event_vid = threading.Event()
+    event_com = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        executor.submit(producer, youtube, 'David Goggins', queue_videos, event_vid)
+        executor.submit(consumer, youtube, queue_videos, queue_comments, event_vid, event_com)
+        executor.submit(writer, queue_comments, event_com)
+        time.sleep(0.1)
+        logging.info("Main: about to set video event")
+        event_vid.set()
+        time.sleep(0.1)
+        logging.info("Main: about to set comments event")
+        event_com.set()
+
+    t1: float = time.perf_counter()
+    print(f"Done in {t1 - t0}")
